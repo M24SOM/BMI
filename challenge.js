aa8989c89ebccc4d1ecf06cf1b95ced91473be85
Mohamed Mahad Farah <mmfarah@proton.me>
2023-05-27 12:21:51 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculateBMI(weight, height) {
+  let bmi = weight / (height * height);
+  return bmi;
+}
+
+let aliBMI = calculateBMI(70, 1.8); // calculate BMI for Ali
+let mohamedBMI = calculateBMI(80, 1.75); // calculate BMI for Mohamed
+
+if (aliBMI > mohamedBMI) {
+  console.log("Ali's BMI is higher than Mohamed's BMI");
+} else if (mohamedBMI > aliBMI) {
+  console.log("Mohamed's BMI is higher than Ali's BMI");
+} else {
+  console.log("Ali's BMI is the same as Mohamed's BMI");
+}
